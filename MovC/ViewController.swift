52cad81318b5d383933603f268ea0a5be9e21d10
Abdusa Alam <abdusa.alam@gmail.com>
2021-03-04 10:59:42 +0700

    Initial commit

---
+++ MovC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovC
+//
+//  Created by Abdusa Alam on 04/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
